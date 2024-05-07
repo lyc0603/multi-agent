9b78466ddccd1a495ffec8fae8baa766f22bd0c8
lyc0603 <luoyichencn110110@gmail.com>
2024-05-07 13:37:15 +0000

    update

--- scripts/lc.py
+++ scripts/lc.py
@@ -2,12 +2,35 @@
 Script to implement a langgraph
 """
 
+import os
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_community.utilities.serpapi import SerpAPIWrapper
+from environ.constants import SERP_KEY
 
-from environ.constants import OPENAI_KEY, SERP_KEY
+print(os.environ["OPENAI_API_1"])
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key=OPENAI_KEY)
-search = SerpAPIWrapper(serpapi_api_key=SERP_KEY)
-search.run("Obama's first name")
+# from environ.constants import OPENAI_KEY, SERP_KEY
+
+# llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key=OPENAI_KEY)
+# search = SerpAPIWrapper(serpapi_api_key=SERP_KEY)
+# search.run("Obama's first name")
+
+# @tool("websearch")
+# def web_search(query: str) -> str:
+#     """
+#     Search with Google Serp API
+#     """
+
+#     search = SerpAPIWrapper(serpapi_api_key=SERP_KEY)
+#     return search.run(query)
+
+# @tool("twitter_writer")
+# def write_tweet(text: str) -> str:
+#     """
+#     Base a piece of content, write a tweet
+#     """
+#     chat = ChatOpenAI(
+#         model="gpt-3.5-turbo-0125",
+#         api_key=OPENAI_KEY
+#     )
