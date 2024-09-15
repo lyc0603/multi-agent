9e8e07be5e612d959221a48c61e11254b908b2f3
lyc0603 <luoyichencn110110@gmail.com>
2024-09-15 11:36:43 +0000

    adjust font size for portfolio figure

--- scripts/plot/portfolio.py
+++ scripts/plot/portfolio.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 from environ.constants import PROCESSED_DATA_PATH, FIGURE_PATH
 
+FONT_SIZE = 14
+
 PLOT_PARAM_MAP = {
     "long_short_adj": {
         "label": "Command",
@@ -36,9 +38,13 @@ for col in ["long_short_adj", "cmkt"]:
         linestyle=PLOT_PARAM_MAP[col]["linestyle"],
     )
 
-plt.legend(frameon=False)
+plt.legend(frameon=False, fontsize=FONT_SIZE)
 plt.grid()
-plt.ylabel("Cumulative Return")
+plt.ylabel("Cumulative Return", fontsize=FONT_SIZE)
+plt.xlabel("Time", fontsize=FONT_SIZE)
+plt.xticks(fontsize=FONT_SIZE)
+plt.yticks(fontsize=FONT_SIZE)
 plt.xlim(df_res.index[0], df_res.index[-1])
+plt.tight_layout()
 plt.savefig(FIGURE_PATH / "portfolio.pdf")
 plt.show()
